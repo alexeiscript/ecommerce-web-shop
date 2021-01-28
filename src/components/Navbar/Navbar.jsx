c17a276c631e72686ff6d8d443d24859ecd651b0
alexeiscript <suzakukururug1@yandex.com>
2021-01-28 16:04:23 +0200

    fetching products from commerce.js

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/shop.png'
 import useStyles from './styles'
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles()
     return (
         <>
@@ -13,12 +13,12 @@ const Navbar = () => {
                 <Toolbar>
                     <Typography variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Alexey Store" height="25px" className={classes.image} />
-                        Alexey Store
+                        Atlantis Shop
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
                         <IconButton aria-label="Show cart items" color="inherit">
-                            <Badge badgeContent={2} color="secondary">
+                            <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -3,12 +3,7 @@ import Product from './Product/Product'
 
 import useStyles from './styles'
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Nike Shoes', price: '$50', image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi3.stycdn.net%2Fimages%2F2012%2F12%2F51%2Farticle%2Fnike%2Fso55402702%2Fnike-eastham-schuhe-grey-white-940-zoom-0.jpg&f=1&nofb=1'},
-    {id: 2, name: 'Laptop', description: 'ASUS Laptop', price: '$999', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.asus.com%2Fid%2FLaptops%2FASUS-Laptop-E402WA%2Fwebsites%2Fglobal%2Fproducts%2F3tcXKZ5TbCLp2epf%2Fimg%2F01%2Ffg.png&f=1&nofb=1'}
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles()
     if (!products.length) return <p>Loading...</p>;
 
@@ -18,7 +13,7 @@ const Products = () => {
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
