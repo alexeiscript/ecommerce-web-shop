2f21a6b14daa3a4a5bf6e6ab05a59d82c34bff6d
alexeiscript <suzakukururug1@yandex.com>
2021-02-04 20:30:55 +0200

    redirect to home page if cart error

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -24,12 +24,13 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
-                <Typography variant="h4">Subtotal: { cart.subtotal.formatted_with_symbol }</Typography>
+                <Typography variant="h5">Subtotal: { cart.subtotal.formatted_with_symbol }</Typography>
                 <div>
                     <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
             </div>
+            <br />
         </>
     )
 
@@ -38,7 +39,7 @@ const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant="h4" gutterBottom>Your Shopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
